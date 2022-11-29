be2565d8e5a7304c41ff69973cdecd002558bcba
Emran Sabbagh <emran2156@gmail.com>
2022-11-29 11:59:29 +0000

    Added the slide to delete feature

--- app/src/main/java/ie/wit/onlineshop/activities/ProductListActivity.kt
+++ app/src/main/java/ie/wit/onlineshop/activities/ProductListActivity.kt
@@ -4,14 +4,13 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
-import android.view.ViewGroup
+import android.view.*
 import android.widget.SearchView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import ie.wit.onlineshop.R
 import ie.wit.onlineshop.adapters.OnlineshopAdapter
 import ie.wit.onlineshop.adapters.OnlineshopListener
@@ -41,27 +40,7 @@ class ProductListActivity : AppCompatActivity(), OnlineshopListener {
 
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
-//        // on below line we are adding on query
-//        // listener for our search view.
-//        searchFilter.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-//            override fun onQueryTextSubmit(query: String?): Boolean {
-//                // on below line we are checking
-//                // if query exist or not.
-//
-//                return false
-//            }
-//            override fun onQueryTextChange(newText: String?): Boolean {
-//                // if query text is change in that case we
-//                // are filtering our adapter with
-//                // new text on below line.
-//                val str:String
-//                if(newText.isNullOrBlank())
-//                    str = ""
-//                else str = newText
-//                //binding.recyclerView.adapter =)
-//                return true
-//            }
-//        })
+
 
         searchFilter.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -76,6 +55,39 @@ class ProductListActivity : AppCompatActivity(), OnlineshopListener {
         //binding.recyclerView.adapter = OnlineshopAdapter(app.products)
         adapter = OnlineshopAdapter(app.products.findAll(),this)
         binding.recyclerView.adapter = adapter
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                // this method is called
+                // when the item is moved.
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // this method is called when we swipe our item to right direction.
+                // on below line we are getting the item at a particular position.
+                val deletedProduct: OnlineshopModel =
+                    app.products.findAll()[viewHolder.adapterPosition]
+
+                // below line is to get the position
+                // of the item at that position.
+                val position = viewHolder.adapterPosition
+
+                // this method is called when item is swiped.
+                // below line is to remove item from our array list.
+                app.products.delete(deletedProduct)
+
+                // below line is to notify our item is removed from adapter.
+                adapter.notifyItemRemoved(viewHolder.adapterPosition)
+
+            }
+            // at last we are adding this
+            // to our recycler view.
+        }).attachToRecyclerView(binding.recyclerView)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
